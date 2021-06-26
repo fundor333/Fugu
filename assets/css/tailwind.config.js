13e67c5f278984490df9ab89e8e6802200f655ac
Fundor333 <fundor333@fundor333.com>
2021-06-26 16:41:14 +0200

    Base of the theme

---
+++ assets/css/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	theme: {
+		colors :{
+			black: '#1f1f1f',
+			white: '#c2c2c2',
+			red: "#8d0801"
+		},
+	  extend: {}
+	},
+	variants: {},
+	plugins: [],
+	darkMode: 'media',
+      }
+      
